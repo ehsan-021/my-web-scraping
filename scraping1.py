2cc019173e567bd5cba372e46b9f3da447f2393f
ehsan <ehhemati85@gmail.com>
2026-08-24 12:46:10 +0330

    in this point my code can get all car name,prise,description,location but just until (agahi haye bishtar) button

--- scraping1.py
+++ scraping1.py
@@ -5,57 +5,33 @@ from selenium.webdriver.common.by import By
 import time
 import pandas
 
-# مسیر chromedriver را مشخص کنید
-chrome_driver_path = "/usr/bin/chromedriver"  # مسیری که در مرحله‌ی قبل پیدا کردید
+chrome_driver_path = "/usr/bin/chromedriver"  
 
 
-
-
-# ایجاد سرویس و مرورگر
 service = Service(executable_path=chrome_driver_path)
 driver = webdriver.Chrome(service=service)
-driver.get("https://divar.ir/s/tehran/car/pride/sedan/petrol?q=%D9%85%D8%A7%D8%B4%DB%8C%D9%86&sort=sort_date")
-
-last_highet=driver.execute_script("return document.body.scrollHeight")
-while True:
-   
-    
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    try:
-            button = driver.find_element(By.CLASS_NAME,"kt-button kt-button--primary kt-nav-button nav-bar__submit-btn-fbb11")
-            button.click()
-            time.sleep(2)
-            print("button clicked")
-    except:
-            pass
-    time.sleep(2)
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    if new_height == last_highet:
-        print("break")
-        break
-    print(last_highet==new_height)
-
-    last_highet=new_height
-
-
-
-
-
+driver.get("https://divar.ir/s/tehran/car?q=%D8%B3%D9%85%D9%86%D8%AF%20lx")
 car_name=[]
 car_describtion=[]
 car_prise=[]
 car_location=[]
-car_names=driver.find_elements(By.CLASS_NAME,"kt-post-card__title")
-car_info=driver.find_elements(By.CLASS_NAME,"kt-post-card__description")
-car_loc=driver.find_elements(By.CLASS_NAME,"kt-post-card__bottom-description")
-counter=0
-for i in car_loc:
+
+
+
+
+last_highet=driver.execute_script("return document.body.scrollHeight")
+while True:
+    car_names=driver.find_elements(By.CLASS_NAME,"kt-post-card__title")
+    car_info=driver.find_elements(By.CLASS_NAME,"kt-post-card__description")
+    car_loc=driver.find_elements(By.CLASS_NAME,"kt-post-card__bottom-description")
+    counter=0
+    for i in car_loc:
        car_location.append(i.text)
-for i in car_names:
-    car_name.append(i.text)
+    for i in car_names:
+       car_name.append(i.text)
    
 
-for i in car_info:     
+    for i in car_info:     
       if counter%2==0:
               car_describtion.append(i.text)
               counter=counter+1
@@ -63,15 +39,37 @@ for i in car_info:
               car_prise.append(i.text)
               counter=counter+1 
 
+    
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        
+
+
+          
+    time.sleep(2)
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_highet:
+        print("break")
+       
+        break
+    print(last_highet,new_height)
+
+    last_highet=new_height
+
+
+
+
+
+
+
 
 
 
 
                
-print(car_name)
-print(car_describtion)
-print(car_prise)
-print(car_location)
+print(len(car_name),"car name:",car_name)
+print(len(car_describtion),"car description:",car_describtion)
+print(len(car_prise),"car price:",car_prise)
+print(len(car_location),"car location:",car_location)
 data=[]
 for a,b,c,d in zip(car_name,car_describtion,car_prise,car_location):
        d={
